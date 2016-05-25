2916e7402d179b1273c9f059889df654812a443c
Misha <novopashinmm@mail.ru>
2016-05-25 12:45:53 +0300

    Докинул файлы, которые не докинул TFS

---
+++ Authors/Models/AuthorBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Authors.Models
+{
+    public class AuthorBook
+    {
+        [ScaffoldColumn(false)]
+        public int AuthorBookID { get; set; }
+        public int AuthorID { get; set; }
+        public int BookID { get; set; }
+
+        public virtual Author Author { get; set; }
+        public virtual Book Book { get; set; }
+    }
+}
